edc363320cbc02e74cddf6e3b9e2bbd2c3f8a1fb
Sayeedh784 <sayeedh74@gmail.com>
2022-03-30 22:54:39 +0600

    video

--- app/static/app/js/myscript.js
+++ app/static/app/js/myscript.js
@@ -23,21 +23,3 @@ $('#slider1, #slider2, #slider3, #slider4').owlCarousel({
 })
 
 
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
