bcfd5a0b03fb2965a3cb6a7c5f45cf46703d7f5a
Sayeedh784 <sayeedh74@gmail.com>
2022-06-20 20:13:32 +0600

    update

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn startup.wsgi --log-file -
--- startup/urls.py
+++ startup/urls.py
@@ -17,9 +17,14 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.conf.urls import url
+from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("app.urls")),
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
