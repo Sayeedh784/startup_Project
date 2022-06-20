2c730a12c7a4433df607e2142d8f8b62ed394a46
Sayeedh784 <sayeedh74@gmail.com>
2022-06-20 20:48:45 +0600

    updated

--- startup/urls.py
+++ startup/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.urls import re_path as url
 from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
