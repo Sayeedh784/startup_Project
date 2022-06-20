917ff1036967414577037d76bcbab04c2918e44c
Sayeedh784 <sayeedh74@gmail.com>
2022-06-20 20:50:24 +0600

    updated

--- startup/urls.py
+++ startup/urls.py
@@ -22,9 +22,7 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("app.urls")),
-    url(r'^media/(?P<path>.*)$', serve,
-        {'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,
-        {'document_root': settings.STATIC_ROOT}),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),
     
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
